ab353a26b6ee05ae3fef75a9ead1dcd2f1c0c90c
Victor Marques <victor.marques.98v@gmail.com>
2026-02-10 20:27:15 -0300

    first commit, here we have a base

---
+++ src/test/java/com/victor/communication_system/CommunicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.communication_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
